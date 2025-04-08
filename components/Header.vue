4929f8b210cc602bca9cb243c3d607519af05ba2
Dimas Maulana Ichsan <35564104+dimasmaulana99@users.noreply.github.com>
2025-04-09 00:33:22 +0700

    Add a Header Component

---
+++ components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/projects">Projects</NuxtLink></li>
+        <li><NuxtLink to="/blog">Blog</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+      </ul>
+    </nav>
+  </template>
+  
+  <script setup>
+  // No special logic needed yet, 
+  // but you can add refs, reactive data, or composables if you want
+  </script>
+  
+  <style scoped>
+  nav {
+    display: flex;
+    justify-content: center;
+    background-color: #007acc;
+    padding: 16px;
+  }
+  ul {
+    display: flex;
+    gap: 24px;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  a {
+    color: white;
+    font-weight: bold;
+    text-decoration: none;
+  }
+  </style>
+  
